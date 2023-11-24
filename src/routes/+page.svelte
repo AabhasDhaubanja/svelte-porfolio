d3bb278e0f06afcc0bb3a24af2e45e4011885213
AabhasDhaubanja <aabhas41@gmail.com>
2023-11-24 19:18:05 +0545

    feat: add sitemap.xml

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,6 +7,10 @@
 
 <svelte:head>
   <title>Aabhas Dhaubanja</title>
+  <meta
+    name="description"
+    content="I'm a full-stack web developer with a passion for creating beautiful and functional web experiences. I'm skilled in both front-end and back-end development, and I'm always up-to-date on the latest technologies. I love the challenge of taking a project from concept to completion, and I'm always looking for new ways to improve my skills and knowledge."
+  />
 </svelte:head>
 <main>
   <Intro />
